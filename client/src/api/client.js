87da6e504c89fa54c1c0c50ed5b05f89594b29f3
kanishkzzz <kanishk8negi@gmail.com>
2026-07-13 20:51:34 +0530

    Added Laminar Tracing

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://pr-review-intelligence-agent.onrender.com",
-  headers: { "Content-Type": "application/json" },
-});
+// const api = axios.create({
+//   baseURL: "http://localhost:8000",
+//   headers: { "Content-Type": "application/json" },
+// });
 
-export const reviewPR = async (prUrl, token) => {
-  const res = await api.post("/review", { pr_url: prUrl, token });
-  return res.data;
-};
+// export const reviewPR = async (prUrl, token) => {
+//   const res = await api.post("/review", { pr_url: prUrl, token });
+//   return res.data;
+// };
